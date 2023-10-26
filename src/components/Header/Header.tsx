0b918d95eecb6d29cbfa1f8dd9ee88575f24fce5
Dima <D130188@yandex.ru>
2023-10-26 18:19:51 +0300

    added logo

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -5,6 +5,7 @@ import { IPropsFn } from "../../types/interfaces";
 function Header({ openModal }: IPropsFn) {
   return (
     <header className="header">
+      <h1 className="header__logo">memo</h1>
       <AddButton openModal={openModal} />
     </header>
   );
